843aea80283c66279d1367bb7f5129c38c1319a4
mekhatib <melkhatib@ziggo.nl>
2021-04-26 22:27:24 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "random_string" "unique" {
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "aks-${random_string.unique.result}"
   location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = var.rg_name
   kubernetes_version  = var.aks_version
   dns_prefix          = "aks"
 
