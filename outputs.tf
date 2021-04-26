89948d903a275f16842894d413cc3497ee90490f
mekhatib <melkhatib@ziggo.nl>
2021-04-26 22:29:43 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
-output rg_name {
-  value = azurerm_resource_group.rg.name
-}
+#output rg_name {
+#  value = azurerm_resource_group.rg.name
+#}
 
 output aks_cluster_name {
   value = azurerm_kubernetes_cluster.aks.name
