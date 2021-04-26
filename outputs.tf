2554ca9d5acb1c9014f952903fd4bc1b69fce079
mekhatib <melkhatib@ziggo.nl>
2021-04-26 20:59:11 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -6,13 +6,13 @@ output aks_cluster_name {
   value = azurerm_kubernetes_cluster.aks.name
 }
 
-output aad_pod_id_binding_selector {
-  value = var.aad_pod_id_binding_selector
-}
+#output aad_pod_id_binding_selector {
+#  value = var.aad_pod_id_binding_selector
+#}
 
-output key_vault_name {
-  value = azurerm_key_vault.kv.name
-}
+#output key_vault_name {
+#  value = azurerm_key_vault.kv.name
+#}
 
 output tenant_id {
   value = data.azurerm_client_config.current.tenant_id
